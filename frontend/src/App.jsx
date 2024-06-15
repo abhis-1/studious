0fc0daf0a5e2c0dd878839c67e8dc11fc5dc94ce
abhis-1 <abhis1pandey@gmail.com>
2024-06-15 08:54:12 +0530

    improved frontend structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,26 @@
-import React from "react"
-import Subjects from "./subjects/Subjects";
-import Home from "./home/Home";
+import React from "react";
+import Subjects from "./pages/Subjects";
+import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./components/SignUp";
-import About from "./about/About";
-import Contact from "./contact/Contact";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
-
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/subject" element={<Subjects />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-
-
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/subject" element={<Subjects />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
