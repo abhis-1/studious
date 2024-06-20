1baaa111e8d09a85ce5869bff8c5627edab1a0ea
Arshiya Srivastava <arshiyasri21@gmail.com>
2024-06-20 14:06:49 +0530

    Home page buttons modified

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -40,6 +40,7 @@ export default function Signin() {
       <div className="bg-slate-300 h-screen flex justify-center">
         <div className="flex flex-col justify-center">
           <div className="rounded-lg bg-white w-96 text-center p-2 h-max px-4">
+            
             <Heading label={"Sign in"} />
             <SubHeading
               label={"Enter your credentials to access your account"}
